7a303087cbb6237b9e923eed9b245eccf58fb948
Andes Analytics <158614066+andesanalytics@users.noreply.github.com>
2024-12-18 08:19:10 -0300

    Delete 0 Otros Archivos directory

--- 0 Otros Archivos/Query Francisco Cornejo.sql
+++
@@ -1,6 +0,0 @@
-select * from cargamas.INT_CODIGOS_EMP where ccodigo = 'CPROD_CZUS_CR' and cempres = 28 and cvalEMP = '571';
-
-select * from cargamas.int_codigos_Emp where cempres = '28' and ccodigo ='CPROD_CZUS' and cvalaxis = '298';
-
-select * from cargamas.int_codigos_Emp ;
-where cempres = '28' and ccodigo ='CPROD_CZUS' ;
